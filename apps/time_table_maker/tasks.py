fb79916d9cb7b84efff035489e76e28e95b5dcaf
Meysam9472 <meysammehmannavaz94@gmail.com>
2026-05-20 18:48:38 +0330

    Fix a problem

--- apps/time_table_maker/tasks.py
+++ apps/time_table_maker/tasks.py
@@ -1,8 +1,8 @@
-from services.time_table_service import time_table_maker
-from celery import shared_task
+from .services.time_table_service import time_table_maker
+from celery_worker import celery_app
 
 
-@shared_task(bind=True, max_retries=5)
+@celery_app.task(bind=True, max_retries=5)
 def time_table_maker_task(self, teachers_data, courses_data, num_rooms, cohorts, days, hours):
     result = time_table_maker(teachers_data, courses_data, num_rooms, cohorts, days, hours)
     return result
